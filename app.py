2cdd322ecc4b14dee70e492bbb2a6b65ebe5bcf8
Jinx <chisel.delahanty@gmail.com>
2019-10-13 13:34:06 +0200

    Set up scraping of Link 3 questions for 1001 and 1002

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ Question A-2 for test 1001 from Link 1 does not work because it relies on an ima
 You can either skip this question or test yourself online here:
 http://passcomptia.com/comptia-a-220-1001/comptia-a-220-1001-question-a-2/
 
-Also, the program runs slowly by necessity (see time.sleep on line 60).
-This is intentional in order to avoid hitting the limit of requests per minute.
+Also, the program runs slowly by necessity (see time.sleep calls).
+This is intentional in order to avoid hitting the limit of requests per minute, 
+which would block the web scraper from accessing the respective webpage.
 
 STILL TO BE DONE:
 - add 900 series questions (save to separate files)
 - set up link2 (currently being blocked by website, needs work-around)
-- add link3:
-https://certification.comptia.org
 """
 
 sources = {
@@ -25,10 +24,12 @@ sources = {
 	"link3": "https://certification.comptia.org/"
 }
 
+
 def clean_html(raw_html):
 	cleanr = re.compile("<.*?>")
 	clean_text = re.sub(cleanr, '', raw_html)
 	return clean_text
+
 
 def save(data):
 	test_num = 0
@@ -54,19 +55,20 @@ def save(data):
 		f.write(data["question_id"] + "\n")
 		f.write(data["correct_answer"] + "\n\n")
 
+
 def collect_link1_links(url):
 	soup = BeautifulSoup(requests.get(url).text, "html.parser") # entire HTML of page
 	links = soup.findAll("a")
 	hrefs1000 = []
 	for link in links:
 		if "220-1001-question" in str(link) or "220-1002-question" in str(link):
-			# print("Found a question!")
 			hrefs1000.append(sources["link1"] + link.get("href"))
 	return hrefs1000
 
+
 def scrape_link1(url):
 	all_question_urls = collect_link1_links(url)
-	# print(f"all_question_urls is {len(all_question_urls)} items long.")
+
 	for url in all_question_urls:
 		try:
 			soup = BeautifulSoup(requests.get(url).text, "html.parser")
@@ -87,13 +89,15 @@ def scrape_link1(url):
 			}
 			save(data_item)
 			time.sleep(2)
+			
 		except AttributeError:
 			continue
+
 
 def collect_link2_links(url):
 	pass
 	soup = BeautifulSoup(requests.get(url).text, "html.parser") # entire HTML of page
-	print(soup)
+	# print(soup)
 	links = soup.findAll("a")
 	hrefs = []
 	for link in links:
@@ -102,6 +106,7 @@ def collect_link2_links(url):
 	# for href in hrefs:
 	# 	print(href)
 	return hrefs
+
 
 def scrape_link2(url):
 	pass
@@ -129,18 +134,71 @@ def scrape_link2(url):
 	# 	except AttributeError:
 	# 		continue
 
+
+def scrape_link3(url):
+	if "1001" in url:
+		print("This is a 1001 test.")
+		id_prefix = "1001"
+	if "1002" in url:
+		print("This is a 1002 test.")
+		id_prefix = "1002"
+
+	soup = BeautifulSoup(requests.get(url).text, "html.parser")
+	
+	question_block = soup.findAll(class_="sfContentBlock")[0]
+	answer_block = soup.findAll(class_="sfContentBlock")[1]
+	question_content = [clean_html(str(x)) for x in question_block if str(x).startswith("<p>")]
+	answer_content = [clean_html(str(x)) for x in answer_block if str(x).startswith("<p>")]
+
+	location = 0
+	questions = []
+
+	for n in range(0, len(question_content)//2):
+		questions.append([question_content[location], question_content[location+1]])
+		location = location + 2
+
+	for item in questions:
+		question_split = [item[0].split("\n", 1)[0], item[0].split("\n", 1)[1]]
+		question_id = question_split[0][9:]
+		print(f"Saving question {question_id}...")
+		question_id_string = f"Test {id_prefix}: Question {question_id}"
+		question = question_split[1]
+		answer_options_list = item[1].split("\n")
+		correct_answer = answer_content[questions.index(item)]
+
+		data_item = {
+			"question_id": question_id_string,
+			"question": question,
+			"answer_options": answer_options_list,
+			"correct_answer": correct_answer
+		}
+
+		save(data_item)
+		time.sleep(2)
+
+
 def scrape(url):
 	if sources["link1"] in url:
-		# print("That's a Link 1 link!")
+		print("Link 1 link detected.")
 		scrape_link1(url)
 	if sources["link2"] in url:
+		print("Link 2 link detected.")
 		scrape_link2(url)
+	if sources["link3"] in url:
+		print("Link 3 link detected.")
+		scrape_link3(url)
+
 
 def run_app():
 	print("Working... this may take a few minutes.")
 
+	# Link 1 links
 	scrape("http://passcomptia.com/comptia-a-220-1001/")
 	scrape("http://passcomptia.com/comptia-a-220-1002/")
+
+	# Link 3 links
+	scrape("https://certification.comptia.org/training/resources/practice-tests/comptia-a-1001-practice-questions")
+	scrape("https://certification.comptia.org/training/resources/practice-tests/comptia-a-1002-practice-questions")
 
 	# url = 'https://www.examcompass.com/comptia-a-plus-certification-practice-test-1-exam-220-1001'
 	# headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"}
@@ -149,10 +207,8 @@ def run_app():
 
 	# scrape("https://www.examcompass.com/comptia-a-plus-certification-practice-test-1-exam-220-1001")
 
-	# scrape("https://certification.comptia.org/training/resources/practice-tests/comptia-a-1001-practice-questions")
-	# scrape("https://certification.comptia.org/training/resources/practice-tests/comptia-a-1002-practice-questions")
-
 	print("All done!")
+
 
 run_app()
 
